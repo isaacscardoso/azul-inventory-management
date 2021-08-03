39ca8be881933cb85cd589b4acf4542bf5be3ee2
isaacscardoso <mukanshinbluedev@outlook.com>
2021-08-03 17:12:08 -0300

    Project created

---
+++ includes/header.php
@@ -0,0 +1,25 @@
+<!doctype html>
+<html lang="pt-br">
+<head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <!-- Bootstrap CSS -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet"
+          integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+
+    <title>Azul</title>
+</head>
+<body class="bg-dark text-light">
+
+    <div class="container">
+        <div class="jumbotron bg-danger p-3 my-3">
+            <h1>Gerenciamento de Estoque</h1>
+        </div>
+
+
+    <!-- listing -->
+    <!-- body -->
+    <!-- footer -->
+</html>
---
+++ includes/listing.php
@@ -0,0 +1,9 @@
+<main>
+    <section>
+        <a href="register.php">
+            <button class="btn btn-success">Adicionar Produto</button>
+        </a>
+    </section>
+</main>
+<!-- body -->
+<!-- footer -->
